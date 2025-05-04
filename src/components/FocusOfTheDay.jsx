a605c5ed5b92dea834119ee8f1cdea5810245417
Thomas OvO <thonnad.thomas@gmail.com>
2025-05-04 18:14:51 +0200

    feat: add logic FocusOfTheDay.jsx with localstorage

--- src/components/FocusOfTheDay.jsx
+++ src/components/FocusOfTheDay.jsx
@@ -1,28 +1,64 @@
-"use client"
+"use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function FocusOfTheDay() {
+  const [focus, setFocus] = useState("");
+  const [submittedFocus, setSubmittedFocus] = useState("");
 
-    const [focus, setFocus] = useState("");
+  useEffect(() => {
+    const savedFocus = localStorage.getItem("focusOfTheDay");
+    if (savedFocus) {
+      setSubmittedFocus(savedFocus);
+    }
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSubmittedFocus(focus);
+    localStorage.setItem("focusOfTheDay", focus);
+    setFocus("");
+  };
   return (
-    <div className="col-span-1 md:col-span-2 bg-blue-600 text-white w-full">
-        <h1 className="text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl text-center">
-          Focus du jour
-        </h1>
-            <form>
-        <div className="w-full flex justify-center items-baseline my-4 space-x-3">
+    <div className="col-span-1 md:col-span-2 flex flex-col justify-center items-center bg-blue-500 text-white w-full rounded-2xl">
+      <h1 className="text-base sm:text-lg md:text-xl lg:text-2xl text-center">
+        Focus du jour 🎯
+      </h1>
+      <form onSubmit={handleSubmit} className="w-full">
+        <div className="w-full flex justify-center items-baseline my-2 space-x-3 flex-wrap">
           <input
             type="text"
-            onChange={(e) => setFocus(e.target.value)}
             value={focus}
+            onChange={(e) => setFocus(e.target.value)}
             placeholder="Quel est ton objectif aujourd'hui ?"
             className="border rounded-lg h-10 p-2 placeholder-white focus:outline-none focus:ring-2 focus:ring-white w-full max-w-xl"
           />
-          <button type="submit" className="p-2 border rounded-lg cursor-pointer ">Go !</button>
+          <button
+            type="submit"
+            className="p-2 border rounded-lg cursor-pointer mt-5 md:mt-0"
+          >
+            Go !
+          </button>
         </div>
-          </form>
-        <p>{focus}</p>
-      </div>
-  )
+      </form>
+      {submittedFocus && (
+        <div className="flex items-baseline justify-center">
+          <p className="text-center text-base sm:text-sm md:text-lg lg:text-xl xl:text-2xl">
+            <strong>Ton focus du jour : {submittedFocus} 🎯</strong>
+          </p>
+          <div className=" flex items-center justify-center ml-2">
+            <button
+              onClick={() => {
+                setSubmittedFocus("");
+                localStorage.removeItem("focusOfTheDay");
+              }}
+              className="h-7 w-7 rounded-lg border text-red-500 cursor-pointer"
+            >
+              X
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }

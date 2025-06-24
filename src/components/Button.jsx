4fc81aabe8ef30d6d524a0751054028ee7034ae2
Thomas OvO <thonnard.thomas@gmail.com>
2025-06-24 17:28:53 +0200

    fix: style Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -11,18 +11,16 @@ export default function Button({
     <button
       {...props}
       className={clsx(
-        // classes communes
         "py-1 px-2 rounded-lg text-lg font-light text-medium cursor-pointer transition-colors duration-300",
 
-        // variantes correspondant aux styles
         {
           default: "text-white border border-green-300 hover:bg-green-300",
-          stop: "bg-red-400 hover:bg-red-500 text-white py-1 px-2 rounded-lg text-sm",
+          stop: "bg-red-400 hover:bg-red-500 text-white py-2 px-4 rounded-lg text-sm md:text-base",
           delete:
-            "bg-red-600 hover:bg-red-700 text-white font-semibold text-base",
+            "bg-red-400 hover:bg-red-500 text-white text-base",
         }[variant],
 
-        className // pour ajouter des classes spécifiques si besoin
+        className
       )}
     >
       {children}
